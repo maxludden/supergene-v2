cdd3299ff06c5a84188678810ee7a95c351130e4
Max Ludden <dev@maxludden.com>
2022-09-06 09:09:47 -0400

    Updat

--- src/chapter.py
+++ src/chapter.py
@@ -3,35 +3,36 @@
 import os
 import pathlib
 import re
-from pathlib import Path
-from typing import Callable, Optional, Union, Any
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from multiprocessing import cpu_count
-from ujson import load, dump
+from pathlib import Path
+from typing import Generic, Type, TypeVar
+from __future__ import annotations
 
-from rich import print
-from rich.console import Console
-from rich.panel import Panel
-from rich.text import Text
-from rich.style import Style
-from mongoengine import Document
+import sh
+from dotenv import load_dotenv
+from mongoengine import Document, QuerySet
 from mongoengine.fields import (
+    DateTimeField,
     EnumField,
     IntField,
     StringField,
     URLField,
-    DateTimeField,
     UUIDField,
 )
-from tqdm.auto import tqdm, trange
-from dotenv import load_dotenv
-import sh
+from rich import print
+from rich.console import Console
+from rich.panel import Panel
+from rich.style import Style
+from rich.text import Text
 from sh import Command
+from tqdm.auto import tqdm, trange
+from ujson import dump, load
 
 touch = Command("touch")
-
 console = Console(width=110)
 
+# > Get Log
 try:
     from log import log
 except ModuleNotFoundError:
@@ -39,7 +40,16 @@ except ModuleNotFoundError:
 except ImportError:
     from src.log import log
 
+# > Get Super Gene & BASE -----------------------------
+try:
+    from atlas import BASE, sg
+except ModuleNotFoundError:
+    from src.atlas import BASE, sg
+except ImportError:
+    from src.atlas import BASE, sg
 
+
+# > Custom Exceptions-----------------------------------
 class ChapterNotFound(ValueError):
     """
     Custom ValueError for when a chapter is not found.
@@ -51,7 +61,6 @@ class ChapterNotFound(ValueError):
 
     pass
 
-
 class SectionNotFound(ValueError):
     """
     Custom ValueError for when a section is not found.
@@ -60,9 +69,7 @@ class SectionNotFound(ValueError):
         `ValueError` (Exception):
             Custom ValueError for when a section is not found.
     """
-
     pass
-
 
 class BookNotFound(ValueError):
     """
@@ -74,7 +81,7 @@ class BookNotFound(ValueError):
     """
     pass
 
-
+# > Error Panel ----------------------------------------
 def error_panel(msg: str, title: str = "Error") -> Panel:
     """
     Create a panel for errors.
@@ -105,9 +112,19 @@ def error_panel(msg: str, title: str = "Error") -> Panel:
     return error_panel
 
 class Chapter(Document):
-    """A class/model representing a chapter in a book."""
+    '''
+    A MongoDB Document class to store chapter data.
+
+    Args:
+        `Document` (MongoEngine.Document):
+            Subclassed from MongoENgine.Document.
+    '''
     chapter = IntField(min_value=1, max_value=3462, required=True, unique=True)
-    section = IntField(min_value = 1, max_value = 17, required = True,)
+    section = IntField(
+        min_value=1,
+        max_value=17,
+        required=True,
+    )
     book = IntField(min_value=1, max_value=10, required=True)
     title = StringField(max_length=500)
     text = StringField()
@@ -119,9 +136,7 @@ class Chapter(Document):
     url = URLField()
     unparsed_text = StringField()
     parsed_text = StringField()
-    meta = {
-        "collection": "chapter",
-    }
+
 
 @log.catch
 def generate_section(chapter: int) -> int | None:
@@ -196,6 +211,7 @@ def generate_section(chapter: int) -> int | None:
             console.print(panel)
             raise ChapterNotFound(msg)
 
+
 @log.catch
 def generate_book(chapter: int) -> int | None:
     """
@@ -252,6 +268,7 @@ def generate_book(chapter: int) -> int | None:
         console.print(panel)
         raise SectionNotFound(msg)
 
+
 def generate_book_dir(chapter: int) -> Path | None:
     """
     Generate the book directory.
@@ -264,7 +281,7 @@ def generate_book_dir(chapter: int) -> Path | None:
     if book:
         if type(book) is int:
             book = int(book)
-            book_zfill = str(book).zfill(2) # example "02"
+            book_zfill = str(book).zfill(2)  # example "02"
             book_dir = Path.cwd() / "books" / f"book{book_zfill}"
             book_dir_panel = Panel(
                 Text(f"{book_dir}", style="bold white on blue"),
@@ -284,6 +301,7 @@ def generate_book_dir(chapter: int) -> Path | None:
         console.print(panel)
         raise BookNotFound(msg)
 
+
 @log.catch
 def generate_filename(chapter: int) -> str | None:
     """
@@ -299,7 +317,7 @@ def generate_filename(chapter: int) -> str | None:
     """
     # > Validate chapter
     if chapter in range(1, 3463):
-        #> Skip non-existent chapter 3095
+        # > Skip non-existent chapter 3095
         if chapter == 3095:
             msg = f"Chapter {chapter} does not exist."
             panel = error_panel(msg, title="Chapter Not Found")
@@ -313,6 +331,7 @@ def generate_filename(chapter: int) -> str | None:
         chapter_zfill = chapter_str.zfill(4)
         filename = f"chapter-{chapter_zfill}"
         return filename
+
 
 @log.catch
 def generate_md_path(chapter: int) -> Path | None:
@@ -336,8 +355,9 @@ def generate_md_path(chapter: int) -> Path | None:
             touch(md_path)
             return md_path
 
+
 @log.catch
-def generate_html_path(chapter: int)-> Path | None:
+def generate_html_path(chapter: int) -> Path | None:
     """
     Generate the path to the HTML file.
 
@@ -360,10 +380,49 @@ def generate_html_path(chapter: int)-> Path | None:
             touch(html_path)
             return html_path
 
+
+def get_url(chapter: int, from_db=True) -> str | None:
+    """
+    Get the URL for the given chapter.
+
+    Returns:
+        `url` (str | None):
+            The URL for the given chapter.
+    """
+    sg()
+    if from_db:
+        chapter_doc = Chapter.objects(chapter=chapter).first()  # type: ignore
+        url = chapter_doc.url
+        if url:
+            return url
+        elif not url:
+            msg = f"URL for chapter {chapter} not found in database."
+            panel = error_panel(msg, title="URL not in MongoDB")
+            console.print(panel)
+
+        else:
+            with open("json/toc.json", "r") as infile:
+                url = dict(load(infile))[str(chapter)]["url"]
+                if url:
+                    return url
+                elif not url:
+                    msg = f"URL for chapter {chapter} not found in TOC.json."
+                    panel = error_panel(msg, title="URL Not Found")
+                    console.print(panel)
+                else:
+                    # > Generate URL
+                    url = (
+                        f"https://bestlightnovel.com/novel_888112448/chapter_{chapter}"
+                    )
+
+                    return url
+
+
 class chapter_gen:
     """
     Generator for chapter_numbers.
     """
+
     def __init__(self, start: int = 1, end: int = 3462):
         self.start = start
         self.end = end
@@ -391,5 +450,3 @@ class chapter_gen:
 
     def __len__(self):
         return self.end - self.start + 1
-
-        

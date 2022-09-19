7d9207cba719aef5677107a4d614a5ea7f0e0ff4
Max Ludden <dev@maxludden.com>
2022-09-18 22:09:07 -0400

    updated titlepages

--- books/book07/md/titlepage-07.md
+++ books/book07/md/titlepage-07.md
@@ -11,5 +11,5 @@ viewport: width=device-width, initial-scale=1.0
 </figure>
 
 
-<p class="title">Written by Twelve Winged Dark Seraphim</p>
-<p class="title">Complied and Edited by Max Ludden</p>
+
+FOOTER
--- src/titlepage.py
+++ src/titlepage.py
@@ -1,8 +1,9 @@
 # core/titlepage.py
 
-import ujson
 from pathlib import Path
 from typing import Optional
+
+import ujson
 from mongoengine import Document
 from mongoengine.fields import IntField, StringField
 from num2words import num2words
@@ -10,11 +11,11 @@ from rich import inspect, print
 from rich.markdown import Markdown
 from rich.panel import Panel
 from rich.progress import Progress
-from rich.text import Text
 from rich.style import Style
+from rich.text import Text
 from sh import Command, RunningCommand
 
-import myaml
+import src.myaml
 from src.atlas import sg
 from src.log import BASE, console, log, logpanel
 
@@ -22,13 +23,19 @@ from src.log import BASE, console, log, logpanel
 # .│                            Titlepage                            │.#
 # .└─────────────────────────────────────────────────────────────────┘.#
 
+
 class MMDConversionException(Exception):
     pass
+
 
 class TitlepageNotFound(Exception):
     pass
 
-TEXT = '### Written by Twelve Winged Dark Seraphim\n ### Complied and Edited by Max Ludden'
+
+TEXT = (
+    "### Written by Twelve Winged Dark Seraphim\n ### Complied and Edited by Max Ludden"
+)
+
 
 class Titlepage(Document):
     book = IntField(required=True, unique=True, min_value=1, max_value=10)
@@ -44,7 +51,15 @@ class Titlepage(Document):
     def __int__(self):
         return self.book
 
-def titlepage_panel(book: int, key: str = "Key", value: str | int | Markdown = "Value", line: int = 1, title: Optional[str] = None, get: bool = False, width = 60) -> Panel:
+
+def titlepage_panel(
+    book: int,
+    key: str = "Key",
+    value: str | int | Markdown = "Value",
+    line: int = 1,
+    title: Optional[str] = None,
+    get: bool = False
+) -> Panel:
     if title:
         title = title
     else:
@@ -63,6 +78,7 @@ def titlepage_panel(book: int, key: str = "Key", value: str | int | Markdown = "
         subtitle_align="right",
         border_style=Style(color="#5f00ff"),
         expand=False,
+        width=80,
     )
     return panel
 
@@ -83,31 +99,30 @@ def generate_titlepage_filename(book: int, save: bool = True) -> str:
 
     """
     book_zfill = str(book).zfill(2)
-    filename = f'titlepage-{book_zfill}'
+    filename = f"titlepage-{book_zfill}"
 
     if save:
         sg()
-        doc = Titlepage.objects(book=book).first() # type: ignore # type: ignore
+        doc = Titlepage.objects(book=book).first()  # type: ignore # type: ignore
         doc.filename = filename
         doc.save()
-        log.debug(f"Saved Book {book}'s titlepage to MongoDB. Filename:<code>\n{filename}</code>")
+        log.debug(
+            f"Saved Book {book}'s titlepage to MongoDB. Filename:<code>\n{filename}</code>"
+        )
 
-    console.print(
-        titlepage_panel(book, "Filename", filename, 96), highlight=True
-    )
+    console.print(titlepage_panel(book, "Filename", filename, 96), highlight=True)
     return filename
 
 
 def get_titlepage_filename(book: int) -> str:
     sg()
-    doc = Titlepage.objects(book=book).first() # type: ignore # type: ignore
+    doc = Titlepage.objects(book=book).first()  # type: ignore # type: ignore
     if doc is None:
         raise TitlepageNotFound(f"Titlepage for Book {book} not found.")
     console.print(
-        titlepage_panel(book, "Filename", doc.filename, 107, get=True),highlight=True
+        titlepage_panel(book, "Filename", doc.filename, 107, get=True), highlight=True
     )
     return doc.filename
-
 
 
 def generate_titlepage_md_path(book: int, save: bool = True) -> Path:
@@ -127,33 +142,33 @@ def generate_titlepage_md_path(book: int, save: bool = True) -> Path:
     """
     book_zfill = str(book).zfill(2)
     filename = generate_titlepage_filename(book)
-    md_path = f'{BASE}/books/book{book_zfill}/md/{filename}.md'
+    md_path = f"{BASE}/books/book{book_zfill}/md/{filename}.md"
 
     if save:
         sg()
-        doc =  Titlepage.objects(book=book).first() # type: ignore
+        doc = Titlepage.objects(book=book).first()  # type: ignore
         doc.md_path = md_path
         doc.save()
-        log.debug(f"Saved Book {book}'s titlepage to MongoDB. Markdown path:<code>\n{md_path}</code>")
+        log.debug(
+            f"Saved Book {book}'s titlepage to MongoDB. Markdown path:<code>\n{md_path}</code>"
+        )
 
-    console.print(
-        titlepage_panel(book, "Markdown Path", md_path, 140), highlight=True
-    )
+    console.print(titlepage_panel(book, "Markdown Path", md_path, 140), highlight=True)
     return Path(md_path)
 
 
 def get_titlepage_md_path(book: int) -> Path:
     sg()
-    doc = Titlepage.objects(book=book).first() # type: ignore
+    doc = Titlepage.objects(book=book).first()  # type: ignore
     if doc.md_path:
 
         console.print(
-            titlepage_panel(book, "Markdown Path", doc.md_path, 151, get=True), highlight=True
+            titlepage_panel(book, "Markdown Path", doc.md_path, 151, get=True),
+            highlight=True,
         )
         return Path(doc.md_path)
     else:
         return generate_titlepage_md_path(book)
-
 
 
 def generate_titlepage_html_path(book: int, save: bool = True):
@@ -172,26 +187,25 @@ def generate_titlepage_html_path(book: int, save: bool = True):
 
     """
     book_zfill = str(book).zfill(2)
-    html_path = f'{BASE}/books/book{book_zfill}/html/titlepage-{book_zfill}.html'
+    html_path = f"{BASE}/books/book{book_zfill}/html/titlepage-{book_zfill}.html"
     if save:
         sg()
-        doc = Titlepage.objects(book=book).first() # type: ignore
+        doc = Titlepage.objects(book=book).first()  # type: ignore
         if doc:
             doc.html_path = html_path
             doc.save()
-            log.debug(f"Saved Book {book}'s titlepage to MongoDB. HTML path:<code>\n{html_path}</code>")
+            log.debug(
+                f"Saved Book {book}'s titlepage to MongoDB. HTML path:<code>\n{html_path}</code>"
+            )
         else:
             raise TitlepageNotFound(f"Titlepage for Book {book} not found.")
 
-    console.print(
-        titlepage_panel(book, "HTML Path", html_path, 187)
-    )
+    console.print(titlepage_panel(book, "HTML Path", html_path, 187))
     return Path(html_path)
 
 
-
 def get_titlepage_html_path(book: int) -> Path:
-    '''
+    """
     Retrieve the html path for the given book's titlepage.
 
     Args:
@@ -199,22 +213,19 @@ def get_titlepage_html_path(book: int) -> Path:
 
     Returns:
         `html_path` (Path): The path to the given book's titlepage's HTML file.
-    '''
+    """
     sg()
-    doc = Titlepage.objects(book=book).first() # type: ignore
+    doc = Titlepage.objects(book=book).first()  # type: ignore
     if doc.html_path:
 
-        console.print(
-            titlepage_panel(book, "HTML Path", doc.html_path, 208, get=True)
-        )
+        console.print(titlepage_panel(book, "HTML Path", doc.html_path, 209, get=True))
         return Path(doc.html_path)
     else:
         return generate_titlepage_html_path(book)
 
 
-
-def generate_titlepage_md(book: int, save: bool = True, write = True) -> str | None:
-    '''
+def generate_titlepage_md(book: int, save: bool = True, write=True) -> str | None:
+    """
     Generate the markdown for the given book's titlepage.
 
     Args:
@@ -226,10 +237,10 @@ def generate_titlepage_md(book: int, save: bool = True, write = True) -> str | N
 
     Returns:
         `md` (str): The markdown of the given book's titlepage.
-    '''
+    """
     book_zfill = str(book).zfill(2)
     sg()
-    doc = Titlepage.objects(book=book).first() # type: ignore
+    doc = Titlepage.objects(book=book).first()  # type: ignore
     if doc:
         title = doc.title
         text = doc.text
@@ -239,42 +250,42 @@ def generate_titlepage_md(book: int, save: bool = True, write = True) -> str | N
 
         meta = f"---\nTitle: {title}\nBook: {book}\nCSS: ../Styles/styles.css\nviewport: width=device-width, initial-scale=1.0\n..."
 
-        atx = f'\n# {title}\n<br />\n### Book {book_word}<br />{img}\n'
+        atx = f"\n# {title}\n<br />\n### Book {book_word}<br />{img}\n"
 
-        text = 'FOOTER'
+        text = "FOOTER"
 
-        md = f'{meta}{atx}\n\n{text}'
+        md = f"{meta}{atx}\n\n{text}"
 
         doc.filename = generate_titlepage_filename(book)
         log.debug(f"Generated Book {book}'s titlepage's filename.")
 
         doc.md_path = generate_titlepage_md_path(book)
-        log.debug(f'Generated titlepage filepath for book {book}.')
+        log.debug(f"Generated titlepage filepath for book {book}.")
 
         doc.html_path = generate_titlepage_html_path(book)
 
         if save:
-            doc.md = md
+            sg()
+            doc = Titlepage.objects(book=book).first()  # type: ignore
+            doc.md = str(md)
             doc.save()
-            log.debug(f'Saved titlepage markdown for book {book}.')
+            log.debug(f"Saved titlepage markdown for book {book}.")
 
         if write:
-            with open(doc.md_path, 'w') as f:
+            with open(doc.md_path, "w") as f:
                 f.write(md)
-                log.debug(f'Wrote titlepage markdown for book {book} to disk.')
+                log.debug(f"Wrote titlepage markdown for book {book} to disk.")
 
-        console.print(
-            titlepage_panel(book, "Markdown", md, 267), highlight=True
-        )
+        console.print(titlepage_panel(book, "Markdown", md, 268), highlight=True)
         return md
 
 
 def get_titlepage_md(book: int) -> str:
     sg()
-    doc = Titlepage.objects(book=book).first() # type: ignore
+    doc = Titlepage.objects(book=book).first()  # type: ignore
     if doc.md:
         console.print(
-            titlepage_panel(book, "Markdown", Markdown(doc.md), 277, get=True)
+            titlepage_panel(book, "Markdown", Markdown(doc.md), 278, get=True)
         )
         return doc.md
 
@@ -283,7 +294,7 @@ def get_titlepage_md(book: int) -> str:
 
 
 def generate_titlepage_html(book: int, save: bool = True) -> str | None:
-    '''
+    """
     Generate the html for the given book's titlepage.
 
     Args:
@@ -295,41 +306,66 @@ def generate_titlepage_html(book: int, save: bool = True) -> str | None:
     Returns:
         `html` (str):
             The html for the given book's titlepage.
-    '''
-    #. Create MMD Command
-    multimarkdown = Command('multimarkdown')
-    mmd = multimarkdown.bake('-f', '--nolables', '-o')
+    """
+    # . Create MMD Command
+    multimarkdown = Command("multimarkdown")
+    mmd = multimarkdown.bake("-f", "--nolabels", "-o")
 
-    #. Connect to MongoDB
+    # . Connect to MongoDB
     sg()
-    doc = Titlepage.objects(book=book).first() # type: ignore
+    doc = Titlepage.objects(book=book).first()  # type: ignore
 
     # > Define save conditions
     def save_titlepage_html(result: RunningCommand | None, book: int) -> str | None:
         sg()
-        doc = Titlepage.objects(book=book).first() # type: ignore
-        if result.exit_code == 0: # type: ignore
-            with open (doc.html_path, 'r') as infile:
+        doc = Titlepage.objects(book=book).first()  # type: ignore
+        if result.exit_code == 0:  # type: ignore
+            with open(doc.html_path, "r") as infile:
                 html = infile.read()
-                html = html.replace('<p>FOOTER</p>', doc.footer)
+                footer = """ 	<div class="footer">
+		<h3>Written by Twelve Winged Dark Seraphim</h3>
+
+		<h3>Formatted and Edited by Max Ludden</h3>
+	</div>"""
+                html = str(html).replace("<p>FOOTER</p>", footer)
                 doc.html = html
                 doc.save()
                 log.debug(f"Saved Book {book}'s titlepage HTML to MongoDB.")
 
                 return html
 
-    doc.md_path = generate_titlepage_md_path(book)
-    doc.html_path = generate_titlepage_html_path(book)
+    doc.md_path = str(generate_titlepage_md_path(book))
+    doc.html_path = str(generate_titlepage_html_path(book))
     doc.save()
-    html = save_titlepage_html(mmd(doc.md_path, doc.html_path), book)
+    html = save_titlepage_html(mmd(doc.html_path, doc.md_path), book)  # type: ignore
     footer = """ 	<div class="footer">
 		<h3>Written by Twelve Winged Dark Seraphim</h3>
 
 		<h3>Formatted and Edited by Max Ludden</h3>
 	</div>"""
-    html = str(html).replace('<p>FOOTER</p>', footer)
-    
-    console.print(
-        titlepage_panel(book, "HTML", f'\n\n{html}', 315)
-    )
+    html = str(html).replace("<p>FOOTER</p>", footer)
+
+    console.print(titlepage_panel(book, "HTML", f"\n\n{html}", 334))
     return html
+
+
+def generate_titlepage_book_word(book: int, save: bool = False) -> str:
+    """
+    Generate the written version of the given book's number.
+
+    Args:
+        `book` (int): The given book.
+
+    Returns:
+        `book_word` (str): The written version of the given book's number.
+    """
+    book_word = str(num2words(book)).capitalize()
+    if save:
+        sg()
+        doc = Titlepage.objects(book=book).first()  # type: ignore
+        doc.book_word = book_word
+        doc.save()
+        log.debug(f"Saved Book {book}'s titlepage's book word to MongoDB.")
+
+    console.print(titlepage_panel(book, "Book Word", book_word, 357))
+    return book_word
